05c2414466b451ec744ed05b5d0be644fbe77fed
unknown <Minskoleopc>
2023-10-25 08:30:21 -0700

    added

---
+++ src/day1.py
@@ -0,0 +1,86 @@
+# comments start with #
+# printing from 1 to 5
+# program 1
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# program 2
+print("hello") # Printing hello
+# Multi-line comments
+# print('Lion')
+# print('Tiger')
+# print('Cat')
+# print('rabbit')
+
+"""
+    This is a multi -line comment in python
+"""
+print("hello")
+
+# program 2
+
+balance = 0
+print(balance)
+balance = 100
+print(balance)
+
+Balance = 10
+balance = 8
+print(Balance)
+print(balance)
+
+q1 = 10
+print('q1')
+q3 = 9
+print('q2')
+print(q3)
+
+# int
+x = 10
+print(x)
+print(type(x))
+# 0 , 12, -45
+
+# float
+y = 89.0
+print(y)
+print(type(y))
+# 78.8 , -78.9 ,0.09
+
+# boolean
+z = True
+print(z)
+print(type(z))
+# True or False
+
+# str
+v = 'chinmay'
+va = "chinmay"
+vab = """chinmay deshpande"""
+vaba = '''vijeet dani'''
+
+print(v)
+print(va)
+print(vab)
+print(vaba)
+print(type(v))
+
+x = '3'
+print(x)
+print(type(x))
+
+x = 3
+print(x)
+print(type(x))
+
+h = '4'
+a = int(h)
+print(a)
+print(type(a))
+
+
+
+
