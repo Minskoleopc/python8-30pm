06264e4042ba5cc9702b19aa4ddc984794317e74
unknown <Minskoleopc>
2023-10-27 08:25:07 -0700

    added

---
+++ src/day3.py
@@ -0,0 +1,112 @@
+#
+# x = 10
+# print(x)
+# print(type(x))
+#
+# y = 12.3
+# print(y)
+# print(type(y))
+#
+# z = True
+# print(z)
+# print(type(z))
+#
+# v = "chinmay"
+# print(v)
+# print(type(v))
+#
+# names = ["chinmay","sarika","poorva","ram"]
+# print(names)
+# print(type(names))
+#
+#
+# cities = ("pune","nagpur")
+# cities2 = "wardha", "nagpur"
+# print(type(cities2))
+# print(type(cities))
+#
+# info = {11,22,33,44,33,44,55}
+# print(info)
+# print(type(info))
+#
+# info2 = {
+#     "firstName":"chinmay",
+#     "lastName":"deshpande"
+# }
+# print(type(info2))
+
+
+# operators -
+
+# Arithmetic operation
+a = 10
+b = 5
+print(a+b)
+firstName = "chinmay"
+lastName = "deshpande"
+print(firstName + lastName)
+
+q1  = 10
+q2 = 5
+
+print(q1+q2)
+print(q1-q2)
+print(q1*q2)
+print(q1/q2)
+print(q1%q2)
+
+# comparison operators
+# entity < entity  ---- boolean  True or False
+# < , >, <= , >= ,== ,!=
+
+print(12 < 4)
+print(12 > 4)
+print(12 !=  4)
+print(12 ==  4)
+print(12 ==  12)
+print(12 >=  12)
+print(12 <=  12)
+print(12 <=  13)
+print(12 >=  13)
+# logical operators
+
+# and
+# True    and   True  ----->  True
+# False   and   True  ----->  False
+# True    and   False  ---->  False
+# False   and   False  -----> False
+
+print(3 == 3 and 4 == 4)
+print(3 != 3 and 4 == 4)
+print(3 == 3 and 4 != 4)
+print(3 != 3 and 4 != 4)
+
+
+# or
+
+# True    or   True  ----->  True
+# False   or   True  ----->  True
+# True    or   False  ---->  True
+# False   or   False  -----> False
+
+print(3 == 3 or 4 == 4)
+print(3 != 3 or 4 == 4)
+print(3 == 3 or 4 != 4)
+print(3 != 3 or 4 != 4)
+
+# not
+#not True --- False
+#not False --- True
+
+print(not(6 == 3))
+print(not (6 == 6))
+
+
+
+
+
+
+
+# conditional statments
+
+
