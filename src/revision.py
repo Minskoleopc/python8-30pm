2483732794e0fc33a0b6482e2f9e1fe83784f653
unknown <Minskoleopc>
2023-10-31 08:25:45 -0700

    added

---
+++ src/revision.py
@@ -0,0 +1,160 @@
+# monday , wednesday , friday , sat , sun - python 8:30pm
+# monday , wednesday , friday , sat , sun - js - 8pm
+# monday , wednesday , friday , sat , sun - java - 9pm
+
+
+#
+# x = 10
+# print(x)
+# x = 100
+# print(x)
+#
+# # Arithmetic operation
+#
+# q = 10
+# r = 8
+#
+# print(q+r)
+# print(q-r)
+# print(q*r)
+# print(q/r)
+# print(q%r)
+#
+# g = 10
+# h = 5
+#
+# print(g+h)
+# print(g-h)
+# print(g*h)
+# print(g/h)
+# print(g*h)
+#
+# def Calculator(x,y):
+#     print(x+y)
+#     print(x - y)
+#     print(x * y)
+#     print(x / y)
+#     print(x % y)
+# Calculator(12,3)
+# Calculator(8,2)
+
+# function without parameter and without  type
+
+def addA():
+    print(9+9)
+addA()
+addA()
+addA()
+addA()
+# function with parameter and without return
+
+def addB(x,y):
+    print(x+y)
+
+addB(12,5)
+addB(15,7)
+
+
+# function with parameter and with return
+def addC(x,y):
+    return x + y
+q1 = addC(13,5)
+print(q1)
+print(q1 - 4)
+print(q1 + q1)
+print(q1 * 2)
+
+# ------------------------------------>
+k = "chinmay"
+print(type(k))
+
+l = 10
+print(type(l))
+
+k = 89.9
+print(type(k))
+
+h1 = [11,22,33,44]
+print(type(h1))
+
+d  = {23,4,455}
+print(type(d))
+
+h2 = {
+    "firstName":"chinmay",
+    "lastName":"deshpande"
+}
+print(type(h2))
+
+j = (12,33)
+j1 = 33,45
+print(type(j1))
+
+
+# comparison
+# < , > , <= , >= , != , ==
+
+print(3 == 3)  # True
+print(3 >= 3)  # True
+print(3 <= 3)  # True
+print(3 != 2)  # True
+print(2 != 2)  # False
+print(2 >= 2)  # True
+print(2 == 2)  # True
+print(3 > 1)   # True
+print(3 < 1)   # False
+
+
+# logical operators
+
+
+# and
+
+
+#   True     and    True   -----> True
+#   True     and    False   -----> False
+#   False     and    True   -----> False
+#   False     and    False   -----> False
+
+print(2 == 2 and 3 == 3)
+print(2 != 2 and 3 == 3)
+print(2 == 2 and 3 != 3)
+print(2 != 2 and 3 != 3)
+
+# or
+
+#   True     or    True   -----> True
+#   True     or   False   -----> True
+#   False    or    True   -----> True
+#   False    or    False   -----> False
+
+
+print(2 == 2 or 3 == 3)
+print(2 != 2 or 3 == 3)
+print(2 == 2 or 3 != 3)
+print(2 != 2 or 3 != 3)
+
+
+
+# not
+ #not True --- False
+ #not False -- True
+
+print(not (7 == 7))
+print(not (8 == 7))
+
+
+
+
+
+
+
+
+
+# logical operator
+
+
+
+
+
+
