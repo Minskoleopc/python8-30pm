64887b5aa454f15be31a94305eac6e1acedc9ead
unknown <Minskoleopc>
2023-11-01 08:27:43 -0700

    added

---
+++ src/day5.py
@@ -0,0 +1,73 @@
+
+age = 18
+str2 = "can drive" if age >= 18 else "cannot drive"
+print(str2)
+
+# for loop
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# program 2
+# range(startIndex,EndIndex(not included),stepSize)
+for x in range(10):
+    print(x)
+
+# program 3
+for x in range(7):
+    print(x)
+
+for x in range(1,7):
+    print(x)
+
+for x in range(1,7,1):
+    print(x)
+
+# 2  3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+for x in range(2,21,2):
+    print(x)
+
+# print hello 3 times
+for x in range(3):
+    print("hello")
+
+# print 1 to 5
+for x in range(1,6):
+    print(x)
+
+# table of 2
+for x in range(2,21,2):
+    print(x)
+
+# print 5 to 1
+for x in range(5,0,-1):
+    print(x)
+
+# print 5 to 0
+for x in range(5,-1,-1):
+    print(x)
+
+# table of 5 in reverse
+for x in range(50,0,-5):
+    print(x)
+
+
+for x in range(1,6):
+    if x == 3:
+        break
+    print(x) #1 # 2
+
+
+for x in range(1,6):
+    print(x) # 1 # 2 # 3
+    if x == 3:
+        break
+
+
+
+
+
+
+
