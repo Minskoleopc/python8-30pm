185e77098edb2e225892812ab199748450d05bf0
unknown <Minskoleopc>
2023-11-17 07:24:29 -0800

    added

---
+++ src/day7.py
@@ -0,0 +1,128 @@
+
+
+x = 10
+print(x)
+x = 1000
+print(x)
+
+
+# Arithmetic operation
+# + , - , * , / , %
+
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+s = 8
+t = 4
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+# functions
+
+# function without parameter and with return type
+def add():
+    print( 9 + 9)
+add()
+add()
+add()
+
+# function with parameter and without return type
+def addB(x,y):
+    print(x+y)
+addB(12,4)
+addB(9,3)
+addB(10,5)
+# function with parameter and with return type
+
+def addC(x,y):
+    return x + y
+q1 = addC(12,5)
+print(q1)
+print(q1 + 3)
+print(q1 + 1)
+print(q1 + 2)
+
+# comparison operator
+# < , > , == , != , <= , >=  ===> boolean
+print(2 > 1)
+print(2 < 1)
+print(2 == 1)
+print(2 != 1)
+print(2 >= 1)
+print(2 <= 1)
+print(2 <= 2)
+print(2 >= 2)
+
+# logical operator
+# and , or , not
+
+# truth table
+
+# and
+# True   and    True    =======> True
+# False  and    True    =======> False
+# True   and    False   ========> False
+# False  and    False   ========> False
+
+
+print(2 == 2  and 3 == 3)
+print(2 != 2  and 3 == 3)
+print(2 == 2  and 3 != 3)
+print(2 != 2  and 3 != 3)
+
+# or
+# True   or  True   ======> True
+# False  or  True   ======> True
+# True   or   False  ======> True
+# False  or   False  ======> False
+
+print(4 == 4 or 5 == 5)
+print(4 != 4 or 5 == 5)
+print(4 == 4 or 5 != 5)
+print(4 != 4 or 5 != 5)
+
+
+# not
+# not False  =====> True
+# not True   =====> False
+print(not 4 == 4)
+print(not 8 != 8)
+
+# conditional statement and type
+
+# loops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
