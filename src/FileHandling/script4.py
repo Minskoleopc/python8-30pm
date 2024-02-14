42b2f122a6b0c74d54e8162d06c2af1c3ef1ebc0
unknown <Minskoleopc>
2024-02-14 20:52:01 +0530

    added

---
+++ src/FileHandling/script4.py
@@ -0,0 +1,45 @@
+# chinmay mayuri sarika
+# 'chinmay   '
+# 'mayuri    '
+# 'sarika    '
+
+
+#reclen = 10
+
+# with open('cities.bin',"wb") as f:
+#     n = int(input("enter the number of users"))
+#     for i in range(n):
+#         name = input("enter the name") #"chinmay"
+#         name = name + (reclen - len(name)) * ' '
+#         name  = name.encode()
+#         f.write(name)
+
+
+# program 2
+# reclen = 10
+# with open('cities.bin','rb') as f:
+#     n = int(input("which record to read ?")) # 3
+#     f.seek(reclen * (n-1))
+#     str = f.read(reclen)
+#     str = str.decode()
+#     print(str)
+
+
+# program 3 -----> search the name in the file
+
+# program 4 -----> search and modify the record
+
+# program 5 -----> search and delete the record
+
+
+
+
+
+
+
+
+
+
+
+
+
